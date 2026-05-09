821d55a1f70daabe086d0612b8bf6e50efbc607e
BEKs <ebaah46@yahoo.com>
2026-05-09 23:48:02 +0300

    Update mod.rs

--- src/repositories/mod.rs
+++ src/repositories/mod.rs
@@ -5,9 +5,11 @@ pub mod member_repo;
 pub mod service_repo;
 pub mod user_repo;
 
-pub use attendance_repo::AttendanceRepository;
-pub use group_repo::GroupRepository;
-pub use household_repo::HouseholdRepository;
-pub use member_repo::MemberRepository;
-pub use service_repo::ServiceRepository;
-pub use user_repo::{RefreshTokenRepository, UserRepository};
+pub use attendance_repo::{AttendanceRepository, PostgresAttendanceRepository};
+pub use group_repo::{GroupRepository, PostgresGroupRepository};
+pub use household_repo::{HouseholdRepository, PostgresHouseholdRepository};
+pub use member_repo::{MemberRepository, PostgresMemberRepository};
+pub use service_repo::{PostgresServiceRepository, ServiceRepository};
+pub use user_repo::{
+    PostgresRefreshTokenRepository, PostgresUserRepository, RefreshTokenRepository, UserRepository,
+};
